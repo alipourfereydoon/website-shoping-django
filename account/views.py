7feff1bf93ee5cba08217b18a226d47a27797b7a
ali <ali.pourferydoon1983@gmail.com>
2025-03-02 12:08:55 +0300

    add suthenticatin method and login page

---
+++ account/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user =authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    return render(request ,'account/login.html',context={})
