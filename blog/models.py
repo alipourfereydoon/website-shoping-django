22fa10f698ed03e47bc634c09d33474fe6ca1505
ali <ali.pourferydoon1983@gmail.com>
2025-03-10 21:06:27 +0300

    add new photos

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class article(models.Model):
-    title=models.CharField(max_length=50)
+    CHOICE=(
+        ('A','PYTHON'),
+        ('B','DJANGO')
+    )
+    author=models.ForeignKey(User,on_delete=models.SET_DEFAULT,default='1')
+    title=models.CharField(max_length=50,choices=CHOICE,unique_for_date='pub_date')
     body= models.TextField()
     image=models.ImageField(upload_to='images/articles')
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
+    pub_date=models.DateField(default=timezone.now())
 
     def __str__(self):
         return f"{self.title} - {self.body[:30]}"
--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from blog.models import article
 
 def home(request):
-    return render(request,"home_app/index.html", {})
+    articles=article.objects.all()
+    return render(request,"home_app/index.html",context={'articles':articles})
